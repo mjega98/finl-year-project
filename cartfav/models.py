61f3f4beb46e096ddd33c3abd47226a7729e4191
Muhammad Muhammad Inuwa <mminuwaali@gmail.com>
2024-02-24 19:24:50 +0000

    modified the cart and fav models, setup basic views for cart and favorites

--- cartfav/models.py
+++ cartfav/models.py
@@ -7,7 +7,12 @@ User = get_user_model()
 
 class Favorite(models.Model):
     user = models.ForeignKey(User, models.CASCADE)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     product = models.ForeignKey(Product, models.PROTECT)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self) -> str:
         return f"{self.user.username}: {self.product.name.title()}"
@@ -23,7 +28,12 @@ class Cart(models.Model):
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, models.CASCADE)
     amount = models.PositiveIntegerField(default=1)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     product = models.ManyToManyField(Product, blank=True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self) -> str:
         return f"cart item of {self.cart.user.username}"
--- cartfav/views.py
+++ cartfav/views.py
@@ -1,3 +1,37 @@
+from . import models
 from django.shortcuts import render
 
-# Create your views here.
+
+def list_cart_view(request):
+    cart, _  = models.Cart.objects.get_or_create(user=request.user)
+    items = cart.cartitem_set.all()
+    return
+
+def add_to_cart_view(request, id):
+    cart, _  = models.Cart.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        carts = cart.cartitem_set.filter(id=id)
+        ...
+    return
+
+def remove_from_cart_view(request, id):
+    cart, _  = models.Cart.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        ...
+    return
+
+
+def list_favorite_view(request):
+    return
+
+
+def add_to_favorite_view(request, id):
+    if request.method == 'POST':
+        ...
+    return
+
+
+def remove_from_favorite_view(request, id):
+    if request.method == 'POST':
+        ...
+    return
